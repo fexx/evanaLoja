1122588a0e4501dfb631efcd6a2ca4907698c07e
fexx <fexx@192.168.1.47>
2016-03-25 21:18:50 -0300

    #5 - Criando estrutura para telas principais.

--- src/main/java/br/com/evana/loja/main/Main.java
+++ src/main/java/br/com/evana/loja/main/Main.java
@@ -1,10 +1,66 @@
 package br.com.evana.loja.main;
 
-public class Main {
+import br.com.evana.loja.componentes.BotaoAzul;
+import br.com.evana.loja.utils.DiretorioArquivo;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Main extends Application{
+	
+	private static Scene scene;
+	private Stage stage;
+	public static BorderPane borderPanePrincipal = new BorderPane();
+	
 
+	@Override
+	public void start(Stage stage) throws Exception {
+		
+		TabPane tabpane = new TabPane();
+		
+		
+		Tab tabHome = new Tab();
+		tabHome.setText("Home");
+		tabHome.setClosable(false);
+		BorderPane borderPane = new BorderPane();
+		borderPane.setTop(new BotaoAzul("Clique"));
+		tabHome.setContent(borderPane);
+		tabpane.getTabs().add(tabHome);
+		
+		Tab tabEstoque = new Tab();
+		tabEstoque.setText("Estoque");
+		tabEstoque.setClosable(false);
+		tabpane.getTabs().add(tabEstoque);
+		
+		borderPanePrincipal.setTop(tabpane);
+		
+//		BotaoAzul botaoAzul = new BotaoAzul("Clique");
+		
+//		borderPanePrincipal.setTop(botaoAzul);
+		
+		Group root = new Group();
+		scene = new Scene(root);
+		
+		scene.getStylesheets().add(DiretorioArquivo.getInstance().getUrlCSS(DiretorioArquivo.CSS_MAIN));
+		
+		borderPanePrincipal.prefHeightProperty().bind(scene.heightProperty());
+		borderPanePrincipal.prefWidthProperty().bind(scene.widthProperty());
+		root.getChildren().add(borderPanePrincipal);
+		stage.setScene(scene);
+		stage.setMinHeight(500);
+		stage.setMinWidth(1000);
+		stage.setMaximized(true);
+		stage.show();
+		
 	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
 
 }
