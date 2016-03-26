1ca49b2a3c77778a12a21235d9689d714e8696db
fexx <fexx@192.168.1.47>
2016-03-25 21:16:19 -0300

    #3 - Fix

---
+++ src/main/java/br/com/evana/loja/utils/DiretorioArquivo.java
@@ -0,0 +1,33 @@
+package br.com.evana.loja.utils;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class DiretorioArquivo {
+	
+	public static DiretorioArquivo INSTANCE = null;
+	
+	private String dirBase = "/br/com/evana/loja/";
+	
+	public static String CSS_MAIN = "main.css";
+	public static String CSS_BOTAO_AZUL = "componentes/botao_azul.css";
+	
+	public ImageView getUrlImagemView(String uri){
+		ImageView imageView = new ImageView();
+		Image image = new Image(getClass().getResourceAsStream(dirBase+"imagem/"+uri));
+		imageView.setImage(image);
+		return imageView;
+	}
+	
+	public String getUrlCSS(String uri){
+		return getClass().getResource(dirBase+"css/"+uri).toExternalForm();
+	}
+	
+	public static synchronized DiretorioArquivo getInstance(){
+		if (INSTANCE == null) {
+			INSTANCE = new DiretorioArquivo();
+		}
+		return INSTANCE;
+	}
+	
+}
